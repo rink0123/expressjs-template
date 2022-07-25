313753dfaeeb8320688ff8ecca167a480597098d
강민성 <42535733+rink0123@users.noreply.github.com>
2022-07-25 18:44:52 +0900

    .
    .

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,37 +1,45 @@
 const createError = require('http-errors');
 
-module.exports = {
-  notFound: (req, res, next) => {
-    const notFound = new createError.NotFound();
-
-    if (process.env.NODE_ENV === 'dev') {
-      console.error(notFound.stack);
-    } else {
-      console.error(notFound.message);
-    }
-
-    return res
-      .status(notFound.statusCode)
-      .send({
-        code: notFound.statusCode,
-        message: notFound.message,
-        data: {}
-      });
-  },
-
-  errorHandler: (err, req, res, next) => {
-    if (process.env.NODE_ENV === 'dev') {
-      console.error(err.stack);
-    } else {
-      console.error(err.message);
-    }
-
-    return res
-      .status(err.statusCode || 500)
-      .send({
-        code: err.statusCode,
-        message: err.message,
-        data: {}
-      });
-  }
+const requestParam = (req) => {
+  console.info("### Error API Parameters List ###");
+  if (Object.keys(req.params).length !== 0) console.info("params: " + JSON.stringify(req.params));
+  if (Object.keys(req.body).length !== 0) console.info("body: " + JSON.stringify(req.body));
+  if (Object.keys(req.query).length !== 0) console.info("query: " + JSON.stringify(req.query));
 };
+
+const notFound = (req, res, next) => {
+  const notFound = new createError.NotFound();
+
+  if (process.env.NODE_ENV === 'dev') {
+    console.error(notFound.stack);
+    
+  } else {
+    console.error(notFound.message);
+  }
+
+  return res
+    .status(notFound.statusCode)
+    .send({
+      code: notFound.statusCode,
+      message: notFound.message,
+      data: {}
+    });
+};
+
+const errorHandler = (err, req, res, next) => {
+  if (process.env.NODE_ENV === 'dev') {
+    console.error(err.stack);
+  } else {
+    console.error(err.message);
+  }
+
+  return res
+    .status(err.statusCode || 500)
+    .send({
+      code: err.statusCode,
+      message: err.message,
+      data: {}
+    });
+};
+
+module.exports = { notFound, errorHandler };
