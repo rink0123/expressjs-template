477662d4dc848f0b08de673406b1a1a15926e9d9
강민성 <42535733+rink0123@users.noreply.github.com>
2022-07-25 18:54:56 +0900

    .
    .

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,10 +1,10 @@
 const createError = require('http-errors');
 
-const requestParam = (req) => {
-  console.info("### Error API Parameters List ###");
-  if (Object.keys(req.params).length !== 0) console.info("params: " + JSON.stringify(req.params));
-  if (Object.keys(req.body).length !== 0) console.info("body: " + JSON.stringify(req.body));
-  if (Object.keys(req.query).length !== 0) console.info("query: " + JSON.stringify(req.query));
+const reqParams = (req) => {
+  console.info("### Request Parameter ###");
+  console.info('params:', JSON.stringify(req.params));
+  console.info('query:', JSON.stringify(req.query));
+  console.info('body:', JSON.stringify(req.body));
 };
 
 const notFound = (req, res, next) => {
@@ -12,7 +12,6 @@ const notFound = (req, res, next) => {
 
   if (process.env.NODE_ENV === 'dev') {
     console.error(notFound.stack);
-    
   } else {
     console.error(notFound.message);
   }
@@ -29,8 +28,10 @@ const notFound = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'dev') {
     console.error(err.stack);
+    reqParams(req);
   } else {
     console.error(err.message);
+    reqParams(req);
   }
 
   return res
