fd6048e181a55341ab501793e5ba12e4f2540ace
강민성 <42535733+rink0123@users.noreply.github.com>
2022-07-25 18:19:05 +0900

    .
    .

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -2,18 +2,36 @@ const createError = require('http-errors');
 
 module.exports = {
   notFound: (req, res, next) => {
-    res.status(404).send({
-      code: 404,
-      message: 'Not Found',
-      data: {}
-    });
+    const notFound = new createError.NotFound();
+
+    if (process.env.NODE_ENV === 'dev') {
+      console.error(notFound.stack);
+    } else {
+      console.error(notFound.message);
+    }
+
+    return res
+      .status(notFound.statusCode)
+      .send({
+        code: notFound.statusCode,
+        message: notFound.message,
+        data: {}
+      });
   },
 
   errorHandler: (err, req, res, next) => {
-    res.status(err.status || 500).send({
-      code: err.status,
-      message: err.message,
-      data: {}
-    });
+    if (process.env.NODE_ENV === 'dev') {
+      console.error(err.stack);
+    } else {
+      console.error(err.message);
+    }
+
+    return res
+      .status(err.statusCode || 500)
+      .send({
+        code: err.statusCode,
+        message: err.message,
+        data: {}
+      });
   }
 };
--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -1,8 +1,11 @@
+const createError = require('http-errors');
+
 const IndexService = require('../service/index.service');
 
 module.exports = {
   get: async (req, res, next) => {
     try {
+      throw createError.InternalServerError('server errors');
       let results = await IndexService.get();
 
       res.status(200).send(results);
