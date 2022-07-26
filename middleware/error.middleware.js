2d3486a113b973acdb81c8c9ea7cb166fe8ed800
강민성 <42535733+rink0123@users.noreply.github.com>
2022-07-26 10:30:29 +0900

    .
    .

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,46 +1,31 @@
 const createError = require('http-errors');
 
-const reqParams = (req) => {
+const getRequest = (req) => {
   console.info("### Request Parameter ###");
   console.info('params:', JSON.stringify(req.params));
   console.info('query:', JSON.stringify(req.query));
   console.info('body:', JSON.stringify(req.body));
 };
 
+const setResponse = (code, message, data) => {
+  return { code, message, data };
+};
+
 const notFound = (req, res, next) => {
-  const notFound = new createError.NotFound();
-
-  if (process.env.NODE_ENV === 'dev') {
-    console.error(notFound.stack);
-  } else {
-    console.error(notFound.message);
-  }
-
-  return res
-    .status(notFound.statusCode)
-    .send({
-      code: notFound.statusCode,
-      message: notFound.message,
-      data: {}
-    });
+  next(createError(404));
 };
 
 const errorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'dev') {
     console.error(err.stack);
-    reqParams(req);
   } else {
     console.error(err.message);
-    reqParams(req);
   }
 
-  return res
-    .status(err.statusCode || 500)
-    .send({
-      code: err.statusCode,
-      message: err.message,
-      data: {}
-    });
+  getRequest(req);
+
+  res.status(err.statusCode || 500).send(setResponse(err.statusCode, err.message, {}));
+  next();
 };
 
 module.exports = { notFound, errorHandler };
